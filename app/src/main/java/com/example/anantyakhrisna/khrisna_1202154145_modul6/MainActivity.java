1973d4e1a1d4dcb422f5b67750ac528ee003efa5
Anank1ng <35679021+Anank1ng@users.noreply.github.com>
2018-04-01 16:41:22 +0700

    Auth Clear

--- app/src/main/java/com/example/anantyakhrisna/khrisna_1202154145_modul6/MainActivity.java
+++ app/src/main/java/com/example/anantyakhrisna/khrisna_1202154145_modul6/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.anantyakhrisna.khrisna_1202154145_modul6;
 
 import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +16,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.logout:
+                logoutFirebase();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void logoutFirebase() {
+        //logout session dari firebase
+        FirebaseAuth.getInstance().signOut();
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
 }
